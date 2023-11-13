d1d80a4f292aac3bed3df0b4e9e07a940eb747af
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-11-13 08:31:29 -0500

    Added DashboardReservation component

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import DashboardReservation from "./DashboardReservation";
+import { Link, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 /**
  * Defines the dashboard page.
@@ -8,7 +10,9 @@ import ErrorAlert from "../layout/ErrorAlert";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
-function Dashboard({ date }) {
+function Dashboard({ initialDate }) {
+  const [date, setDate] = useState(initialDate)
+  const history = useHistory();
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
 
@@ -23,6 +27,21 @@ function Dashboard({ date }) {
     return () => abortController.abort();
   }
 
+  const cycleDate = (event) => {
+    event.preventDefault();
+    let text = event.target.innerText;
+    let newDate = new Date(date);
+    if (text === "<") {
+      newDate.setDate(newDate.getDate() - 1);
+    }
+    if (text === ">") {
+      newDate.setDate(newDate.getDate() + 1);
+    }
+    newDate = newDate.toISOString().split("T")[0];
+    setDate(newDate)
+    history.push(`/dashboard?date=${newDate}`);
+  };
+
   return (
     <main>
       <h1>Dashboard</h1>
@@ -30,7 +49,15 @@ function Dashboard({ date }) {
         <h4 className="mb-0">Reservations for date</h4>
       </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <div className="col">
+        <button onClick={cycleDate} type="button" className="btn btn-secondary">{"<"}</button>
+        <Link to={"/"} type="button" className="btn btn-primary">Today</Link>
+        <button onClick={cycleDate} type="button" className="btn btn-secondary">{">"}</button>
+      </div>
+      <ol>
+        {reservations.map((reservation, index) => 
+        <DashboardReservation key={index} reservation={reservation} />)}
+      </ol>
     </main>
   );
 }
