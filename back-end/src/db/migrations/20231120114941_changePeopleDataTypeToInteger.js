09d8739c35c3143c54b4f0fbd616df99353fcb4d
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-11-20 12:09:30 -0500

    changed people column data type to integer

---
+++ back-end/src/db/migrations/20231120114941_changePeopleDataTypeToInteger.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex) {
+  return knex.schema.alterTable("reservations", (table) => {
+    table.integer("people").alter()
+  })
+};
+
+exports.down = function(knex) {
+  
+};
