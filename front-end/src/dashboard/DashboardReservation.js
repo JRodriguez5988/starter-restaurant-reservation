13ccb212895d59b6056ed0153f2ebf27c09cf9c1
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-11-23 10:03:23 -0500

    Added "seat" button to each reservation

--- front-end/src/dashboard/DashboardReservation.js
+++ front-end/src/dashboard/DashboardReservation.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
 
 function DashboardReservation({reservation}) {
+    const reservation_id = reservation.reservation_id;
     return (
         <li className="list-group-item">
-            <h6 className="card-header">Name: {reservation.first_name} {reservation.last_name}</h6>
+            <h6 className="card-header">Name: {reservation.first_name} {reservation.last_name} {`(${reservation.mobile_number})`}</h6>
             <div className="card-body">
                 <p>Time: {reservation.reservation_time}</p>
                 <p>Party Size: {reservation.people}</p>
             </div>
+            <Link to={`/reservations/${reservation_id}/seat`} type="button" className="btn btn-primary">Seat</Link>
         </li>
     )
 }
