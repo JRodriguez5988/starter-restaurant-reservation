6bb813aeca2abd7b70f0649c4f0b6c6f2896b855
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-11-28 09:22:00 -0500

    moved styling from Layout.css to inline styling

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -2,8 +2,6 @@ import React from "react";
 import Menu from "./Menu";
 import Routes from "./Routes";
 
-// import "./Layout.css";
-
 /**
  * Defines the main layout of the application.
  *
@@ -15,7 +13,7 @@ function Layout() {
   return (
     <div className="container-fluid">
       <div className="row h-100">
-        <div className="col-md-2 side-bar" style={{backgroundColor: "#393a44"}}>
+        <div className="col-md-2 side-bar" style={{backgroundColor: "#393a44", minWidth: "200px"}}>
           <Menu />
         </div>
         <div className="col">
