bee159b7718a01b9981f1feeed3ea0e174aeaba5
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-12-09 14:47:23 -0500

    added migration for status of reservations

--- back-end/src/db/migrations/20231120114941_changePeopleDataTypeToInteger.js
+++ back-end/src/db/migrations/20231120114941_changePeopleDataTypeToInteger.js
@@ -1,10 +1,12 @@
 
 exports.up = function(knex) {
   return knex.schema.alterTable("reservations", (table) => {
-    table.integer("people").alter()
-  })
+    table.integer("people").alter();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.alterTable("reservations", (table) => {
+    table.string("people").alter();
+  });
 };
