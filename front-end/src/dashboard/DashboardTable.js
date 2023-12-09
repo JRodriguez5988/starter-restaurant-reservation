fd41fdfb93f2c43203ae61fc9eeacb52b4910e29
Joshua Rodriguez <128635750+JRodriguez5988@users.noreply.github.com>
2023-12-09 14:47:58 -0500

    edited DashboardTable component to getStatus

--- front-end/src/dashboard/DashboardTable.js
+++ front-end/src/dashboard/DashboardTable.js
@@ -4,9 +4,8 @@ import { deleteAssignment } from "../utils/api";
 
 function DashboardTable({ table }) {
     const history = useHistory();
-    let status;
 
-    const handleDelete = async (event) => {
+    const handleFinish = async (event) => {
         event.preventDefault();
         if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
           await deleteAssignment(table);
@@ -14,22 +13,20 @@ function DashboardTable({ table }) {
         };
       };
 
-    function isSeated() {
-        let result = true
-        status = "Occupied"
+    function getStatus() {
+        let status = "Occupied"
         if (!table.reservation_id) {
-            result = false;
             status = "Free"
         };
-        return result;
+        return status;
     };
 
     return (
         <div className="list-group-item">
             <h6>{table.table_name}:</h6>
-            <p data-table-id-status={table.table_id}>{!isSeated() ? "Free" : "Occupied"}</p>
-            {status = "Occupied" ?
-            <button className="btn btn-primary" data-table-id-finish={table.table_id} onClick={handleDelete}>Finish</button>
+            <p data-table-id-status={table.table_id}>{getStatus()}</p>
+            {getStatus() === "Occupied" ?
+            <button className="btn btn-primary" data-table-id-finish={table.table_id} onClick={handleFinish}>Finish</button>
             : null} 
         </div>
     );
